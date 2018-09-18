45f6ec928a585e4eac3e6e0cd712d183d3e1416c
Gregg Kellogg <gregg@greggkellogg.net>
2018-09-18 10:31:11 -0700

    Sync common files.

--- common/jsonld.js
+++ common/jsonld.js
@@ -2,30 +2,6 @@ const jsonld = {
   // Add as the respecConfig localBiblio variable
   // Extend or override global respec references
   localBiblio: {
-    "JSON-LD11": {
-      title: "JSON-LD 1.1",
-      href: "https://w3c.github.io/json-ld-syntax/",
-      authors: ["Gregg Kellogg"],
-      publisher: "W3C",
-      status: 'FPWD',
-      date: '11 September 2018'
-    },
-    "JSON-LD11-API": {
-      title: "JSON-LD 1.1 Processing Algorithms and API",
-      href: "https://w3c.github.io/json-ld-api/",
-      authors: ["Gregg Kellogg"],
-      publisher: "W3C",
-      status: 'FPWD',
-      date: '11 September 2018'
-    },
-    "JSON-LD11-FRAMING": {
-      title: "JSON-LD 1.1 Framing",
-      href: "https://w3c.github.io/json-ld-framing/",
-      authors: ["Gregg Kellogg"],
-      publisher: "W3C",
-      status: 'FPWD',
-      date: '11 September 2018'
-    },
     // aliases to known references
     "IEEE-754-2008": {
       title: "IEEE 754-2008 Standard for Floating-Point Arithmetic",
@@ -42,8 +18,8 @@ const jsonld = {
     }
   },
   conversions: {
-    "https://w3c.github.io/json-ld-syntax/": "http://www.w3.org/TR/2018/WD-json-ld11-20180911/",
-    "https://w3c.github.io/json-ld-api/": "http://www.w3.org/TR/2018/WD-json-ld11-api-20180911/",
-    "https://w3c.github.io/json-ld-framing/": "http://www.w3.org/TR/2018/WD-json-ld11-framing-20180911/"
+    "https://w3c.github.io/json-ld-syntax/": "http://www.w3.org/TR/json-ld11/",
+    "https://w3c.github.io/json-ld-api/": "http://www.w3.org/TR/json-ld11-api/",
+    "https://w3c.github.io/json-ld-framing/": "http://www.w3.org/TR/json-ld11-framing/"
   }
 };
